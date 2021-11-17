4fdf308b3520a620c4bb95c937ce488901e4cc8d
G-Salis <giuseppesalislavoro@gmail.com>
2021-11-17 18:54:14 +0100

    MILESTONE_3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+
+d = new Date();
+const time = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+
+
+ let doubleDigitMin = d.getSeconds().toLocaleString('en-US', {
+   minimumIntegerDigits: 2,
+   useGrouping: false
+ })
+
+ 
+ console.log(
+   'Output:   ' + doubleDigitMin
+ )
+
+
+
 const app = new Vue({
   el: "#app",
 
@@ -14,17 +31,17 @@ const app = new Vue({
           {
             data: "01/01/1970 00:00:00",
             text: "Hai portato a spasso il cane?",
-            stato: "ricevuto"
+            stato: "chat-user"
           },
           {
             data: "01/01/1970 00:00:00",
             text: "Ah Scusa!",
-            stato: "inviato"
+            stato: "chat-client"
           },
           {
             data: "01/01/1970 00:00:00",
             text: "Tutto fatto!",
-            stato: "ricevuto"
+            stato: "chat-user"
           },
         ] 
       },
@@ -37,12 +54,12 @@ const app = new Vue({
           {
             data: "01/01/1970 00:00:00",
             text: "Lollatina??",
-            stato: "inviato"
+            stato: "chat-client"
           },
           {
             data: "01/01/1970 00:00:00",
             text: "Mi piacerebbe ma devo andare in palestra!",
-            stato: "ricevuto"
+            stato: "chat-user"
           },
         ] 
       },
@@ -55,12 +72,12 @@ const app = new Vue({
           {
             data: "01/01/1970 00:00:00",
             text: "Ti sei dimenticato i soldi da me",
-            stato: "inviato"
+            stato: "chat-client"
           },
           {
             data: "01/01/1970 00:00:00",
             text: "Ah scusa! Potresti portarmeli oggi?",
-            stato: "ricevuto"
+            stato: "chat-user"
           },
         ] 
       },
@@ -73,18 +90,93 @@ const app = new Vue({
           {
             data: "01/01/1970 00:00:00",
             text: "Oggi puoi uscire? Andiamo al cinema?",
-            stato: "inviato"
+            stato: "chat-client"
           },
           {
             data: "01/01/1970 00:00:00",
             text: "Si, ma preferirei andare al cinema!",
-            stato: "ricevuto"
+            stato: "chat-user"
           },
         ] 
       },
     ],
 
     counter: 0,
+    toSend: '',
+    clock: null
 
-  }
+  },
+
+  methods: {
+    msgSend() {
+
+       if (this.toSend.trim() !== '') {
+
+          let time = '';
+          time = this.getDate(time);
+          
+          const msgStr = {
+            data: '01/01/1970 ' + time,
+            text: this.toSend,
+            stato: 'chat-user'
+          }
+    
+          this.contacts[this.counter].messages.push(msgStr);
+          this.msgAuto()
+ 
+          this.toSend = '';
+       } else {
+          console.log('error');
+       }
+
+       
+       
+    },
+
+    msgAuto(){
+      this.clock = setTimeout(()=>{
+        this.msgRecived();
+      },1000)
+    },
+
+
+    msgRecived() {
+
+      let time = '';
+      time = this.getDate(time);
+
+      const msgStr = {
+        data: '01/01/1970 ' + time,
+        text: "Ok.",
+        stato: "chat-client"
+      }
+
+      this.contacts[this.counter].messages.push(msgStr);
+
+      this.toSend = '';
+
+   },
+
+    getDate(d) {
+       d = new Date();
+
+       let doubleDigitSec = d.getSeconds().toLocaleString('en-US', {
+        minimumIntegerDigits: 2,
+      })
+
+      let doubleDigitMin = d.getMinutes().toLocaleString('en-US', {
+        minimumIntegerDigits: 2,
+      })
+
+      let doubleDigitHou = d.getHours().toLocaleString('en-US', {
+        minimumIntegerDigits: 2,
+      })
+
+       const time = `${doubleDigitHou}:${doubleDigitMin}:${doubleDigitSec}`;
+
+       // console.log(time);
+
+       return time;
+    }
+ }
 })
